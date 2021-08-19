58a379215b7b3c855de6c92a4d7e252d39642902
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-19 15:02:33 +0100

    updated js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 window.onload = async () => {
     try {
-        const response = await fetch("https://striveschool-api.herokuapp.com/api/product/");
-        method: 'GET',
-        headers: {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+            headers: {
                 "Content-Type": "application/json",
-                authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
             }
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data);
-           
-            });
-        }
-    } catch (error) {
-        console.log(error);
+        })
+    } catch (err) {
+        console.log(err)
     }
-};
+}
