72d207de3da077f76fe64c871dbfc8066ba557df
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-19 21:03:38 +0100

    Updated, followed homework code for understanding but getting url is not defined

--- adminscript.js
+++ adminscript.js
@@ -1,3 +1,13 @@
+const params = new URLSearchParams(location.search)
+const id = params.get('id')
+
+window.onload = function () {
+
+    if (id) document.querySelector("#page-title").innerHTML = "Edit product"
+    getProductDetails(id)
+
+}
+
 const handleSubmit = async (event) => {
     event.preventDefault()
     
@@ -26,3 +36,47 @@ const handleSubmit = async (event) => {
         console.log(err)
     }
 }
+
+
+async function deleteProduct() {
+      
+      try {
+          const response = await fetch(url + id, {
+              method: 'DELETE',
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
+            } })
+
+        if (!response.ok) throw new Error("Failed to delete product")
+
+        alert("All good - product deleted successfully.")
+        location.assign("index.html")
+      } catch (error) {
+        alert(error.message)
+      }
+    
+}
+    
+async function getProductDetails(id) {
+     
+      try {
+        const response = await fetch(url + id, {
+          headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
+            }
+        })
+        const product = await response.json()
+
+        console.log(product)
+        console.log(Object.keys(product))
+
+        Object.keys(product).forEach(key => {
+          const field = document.querySelector(`#${key}`)
+          if (field) field.value = product[key]
+        })
+      } catch (error) {
+        alert(error)
+      }
+    }
--- script.js
+++ script.js
@@ -6,7 +6,41 @@ window.onload = async () => {
                 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjllNzJkNTI2MjAwMTViNmRjYTIiLCJpYXQiOjE2MjkzNzE5NDIsImV4cCI6MTYzMDU4MTU0Mn0.Hkx4RB6mhAb2PvDCTK6p7W2Fe7IKxKfGllbs4jWQq84",
             }
         })
+        const fetchedProducts = await response.json()
+        displayProducts(fetchedProducts)
+        //after the fetch, assing the result to a variable called how ever you want, then call displayProducts(theVariableYouCreated)
     } catch (err) {
         console.log(err)
     }
+}
+
+const displayProducts = (products) => {
+    const productSection = document.querySelector(".productSection")
+
+    if (products) {
+        products.forEach((product) => {
+          productSection.innerHTML += `<div class="row">
+            <div class="col-md-4">
+            <div class="card" style="width: 18rem">
+              <img src="${product.imageUrl}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">${product.price}</h5>
+                <p class="card-text font-weight-bold">
+                  ${product.name}
+                </p>
+                <p class="card-text font-weight-bolder">
+                  ${product.brand}
+                </p>
+                <p class="card-text">
+                  ${product.description}
+                </p>
+                <a href="#" class="btn btn-primary">${product.price}</a>
+                <a class="btn btn-warning" href="backoffice.html?id=${product._id}">Edit</a>
+              </div>
+            </div>
+          </div>
+          </div>`
+        })
+    }
+
 }
